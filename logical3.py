6b60ae33d48fea0fc839d09ca80a66c3cfacf71d
farazali-110 <151817304+farazali-110@users.noreply.github.com>
2024-02-12 20:09:45 +0500

    Add files via upload

---
+++ logical3.py
@@ -0,0 +1,8 @@
+num1=eval(input("enter 1 number :"))
+num2=eval(input("enter 2 number :"))
+if num1>num2:
+    print("num1 is greater:")
+elif num1<num2:
+    print("num2 is greater:")
+else:
+    print("both number are equal:")
